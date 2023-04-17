a4db5b5fca298d309e7be16e278da33513c7fd0e
baderp <baderp@gmx.de>
2023-04-17 14:59:23 +0200

    initialize

---
+++ config/system/settings.php
@@ -0,0 +1,188 @@
+<?php
+return [
+    'BE' => [
+        'debug' => false,
+        'installToolPassword' => '$argon2i$v=19$m=65536,t=16,p=1$YUxnLlpOSS96MU9BMkZuaA$KiMLrtlxDlauNCp0avlZEcDnn+ts4Zol6CwCQH+LlhA',
+        'passwordHashing' => [
+            'className' => 'TYPO3\\CMS\\Core\\Crypto\\PasswordHashing\\Argon2iPasswordHash',
+            'options' => [],
+        ],
+    ],
+    'DB' => [
+        'Connections' => [
+            'Default' => [
+                'charset' => 'utf8mb4',
+                'dbname' => 'db',
+                'driver' => 'mysqli',
+                'host' => 'db',
+                'password' => 'db',
+                'port' => 3306,
+                'tableoptions' => [
+                    'charset' => 'utf8mb4',
+                    'collate' => 'utf8mb4_unicode_ci',
+                ],
+                'user' => 'db',
+            ],
+        ],
+    ],
+    'EXTCONF' => [
+        'lang' => [
+            'availableLanguages' => [
+                'de',
+            ],
+        ],
+    ],
+    'EXTENSIONS' => [
+        'backend' => [
+            'backendFavicon' => '',
+            'backendLogo' => '',
+            'loginBackgroundImage' => '',
+            'loginFootnote' => '',
+            'loginHighlightColor' => '',
+            'loginLogo' => '',
+            'loginLogoAlt' => '',
+        ],
+        'extensionmanager' => [
+            'automaticInstallation' => '1',
+            'offlineMode' => '0',
+        ],
+        'ke_search' => [
+            'allowEmptySearch' => '1',
+            'enableExplicitAnd' => '0',
+            'enablePartSearch' => '1',
+            'finishNotification' => '0',
+            'loglevel' => 'ERROR',
+            'multiplyValueToTitle' => '1',
+            'notificationRecipient' => '',
+            'notificationSender' => 'no_reply@domain.com',
+            'notificationSubject' => '[KE_SEARCH INDEXER NOTIFICATION]',
+            'pathCatdoc' => '/usr/bin/',
+            'pathPdfinfo' => '/usr/bin/',
+            'pathPdftotext' => '/usr/bin/',
+            'searchWordLength' => '4',
+        ],
+        'mask' => [
+            'backend' => 'EXT:setup_package/Resources/Private/Mask/Templates/Plugins/Backend/Templates',
+            'backend_layouts_folder' => 'EXT:setup_package/Configuration/Mask/BackendLayouts',
+            'backendlayout_pids' => '0',
+            'content' => 'EXT:setup_package/Resources/Private/Templates/Plugins/Mask/',
+            'content_elements_folder' => 'EXT:setup_package/Configuration/Mask/ContentElements',
+            'json' => '',
+            'layouts' => 'EXT:setup_package/Resources/Private/Templates/Plugins/Mask/',
+            'layouts_backend' => 'EXT:setup_package/Resources/Private/Templates/Plugins/Mask/Backend/Layouts',
+            'loader_identifier' => 'json-split',
+            'partials' => 'EXT:setup_package/Resources/Private/Templates/Plugins/Mask/',
+            'partials_backend' => 'EXT:setup_package/Resources/Private/Templates/Plugins/Mask/Backend/Partials',
+            'preview' => 'EXT:setup_package/Resources/Private/Templates/Plugins/Mask/Backend/Icons',
+        ],
+        'news' => [
+            'advancedMediaPreview' => '1',
+            'archiveDate' => 'date',
+            'categoryBeGroupTceFormsRestriction' => '0',
+            'categoryRestriction' => '',
+            'contentElementRelation' => '1',
+            'dateTimeNotRequired' => '0',
+            'hidePageTreeForAdministrationModule' => '0',
+            'manualSorting' => '0',
+            'prependAtCopy' => '1',
+            'resourceFolderImporter' => '/news_import',
+            'rteForTeaser' => '0',
+            'showAdministrationModule' => '1',
+            'slugBehaviour' => 'unique',
+            'storageUidImporter' => '1',
+            'tagPid' => '1',
+        ],
+        'scheduler' => [
+            'maxLifetime' => '1440',
+        ],
+        'webp' => [
+            'convert_all' => '1',
+            'converter' => 'Plan2net\\Webp\\Converter\\MagickConverter',
+            'hide_webp' => '1',
+            'mime_types' => 'image/jpeg,image/png,image/gif',
+            'parameters' => 'image/jpeg::-quality 85 -define webp:lossless=false|image/png::-quality 75 -define webp:lossless=true|image/gif::-quality 85 -define webp:lossless=true',
+            'silent' => '0',
+            'use_system_settings' => '1',
+        ],
+    ],
+    'FE' => [
+        'debug' => false,
+        'disableNoCacheParameter' => true,
+        'passwordHashing' => [
+            'className' => 'TYPO3\\CMS\\Core\\Crypto\\PasswordHashing\\Argon2iPasswordHash',
+            'options' => [],
+        ],
+    ],
+    'GFX' => [
+        'imagefile_ext' => 'gif,jpg,jpeg,tif,tiff,bmp,pcx,tga,png,pdf,ai,svg,webp',
+        'processor' => 'GraphicsMagick',
+        'processor_allowTemporaryMasksAsPng' => false,
+        'processor_colorspace' => 'RGB',
+        'processor_effects' => false,
+        'processor_enabled' => true,
+        'processor_path' => '/usr/bin/',
+    ],
+    'LOG' => [
+        'TYPO3' => [
+            'CMS' => [
+                'deprecations' => [
+                    'writerConfiguration' => [
+                        'notice' => [
+                            'TYPO3\CMS\Core\Log\Writer\FileWriter' => [
+                                'disabled' => true,
+                            ],
+                        ],
+                    ],
+                ],
+            ],
+        ],
+    ],
+    'MAIL' => [
+        'transport' => 'sendmail',
+        'transport_sendmail_command' => '/usr/local/bin/mailhog sendmail test@example.org --smtp-addr 127.0.0.1:1025',
+        'transport_smtp_encrypt' => '',
+        'transport_smtp_password' => '',
+        'transport_smtp_server' => '',
+        'transport_smtp_username' => '',
+    ],
+    'SYS' => [
+        'UTF8filesystem' => true,
+        'caching' => [
+            'cacheConfigurations' => [
+                'hash' => [
+                    'backend' => 'TYPO3\\CMS\\Core\\Cache\\Backend\\Typo3DatabaseBackend',
+                ],
+                'imagesizes' => [
+                    'backend' => 'TYPO3\\CMS\\Core\\Cache\\Backend\\Typo3DatabaseBackend',
+                    'options' => [
+                        'compression' => true,
+                    ],
+                ],
+                'pages' => [
+                    'backend' => 'TYPO3\\CMS\\Core\\Cache\\Backend\\Typo3DatabaseBackend',
+                    'options' => [
+                        'compression' => true,
+                    ],
+                ],
+                'rootline' => [
+                    'backend' => 'TYPO3\\CMS\\Core\\Cache\\Backend\\Typo3DatabaseBackend',
+                    'options' => [
+                        'compression' => true,
+                    ],
+                ],
+            ],
+        ],
+        'devIPmask' => '',
+        'displayErrors' => 0,
+        'encryptionKey' => 'be2670ce0fdceaa7f41f76b39a8464755632ae15ad72eed88148b1227653fc553fde7fe875cb57f863ee91f68475778a',
+        'exceptionalErrors' => 4096,
+        'sitename' => 'New TYPO3 12',
+        'systemLocale' => 'de_DE.UTF-8',
+        'systemMaintainers' => [
+            1,
+            3,
+            4,
+            17,
+        ],
+    ],
+];
