7c02eab94a549a1bfdcc2baf229dd61de530da02
baderp <baderp@gmx.de>
2023-04-21 16:29:23 +0200

    add Icons.php

---
+++ public/packages/setup_package/Configuration/Icons.php
@@ -0,0 +1,16 @@
+<?php
+return [
+    'icon-1' => [
+        'provider' => \TYPO3\CMS\Core\Imaging\IconProvider\FontawesomeIconProvider::class,
+        'name' => 'icon-1',
+        'spinning' => true
+    ],
+    '3-col' => [
+        'provider' => \TYPO3\CMS\Core\Imaging\IconProvider\SvgIconProvider::class,
+        'source' => 'EXT:setup_package/Resources/Public/Icons/SVGs/Active/3-col.svg'
+    ],
+    'onecol' => [
+        'provider' => \TYPO3\CMS\Core\Imaging\IconProvider\BitmapIconProvider::class,
+        'source' => 'EXT:my_extension/Resources/Public/Icons/onecol.png'
+    ],
+];
